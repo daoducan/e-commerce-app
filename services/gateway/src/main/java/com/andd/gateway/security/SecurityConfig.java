02ba7f3a878265f337f00c775146ddb1bfc27b20
AnDD 1791 <andd1791@gmail.com>
2024-07-11 13:51:40 +0700

    update

--- services/gateway/src/main/java/com/andd/gateway/security/SecurityConfig.java
+++ services/gateway/src/main/java/com/andd/gateway/security/SecurityConfig.java
@@ -2,15 +2,16 @@ package com.andd.gateway.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+//import org.springframework.security.config.web.server.ServerHttpSecurity;
+//import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
-@EnableWebFluxSecurity
+//@EnableWebFluxSecurity
 public class SecurityConfig {
 
+    /*
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttpSecurity) {
         serverHttpSecurity
@@ -23,5 +24,5 @@ public class SecurityConfig {
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
         return serverHttpSecurity.build();
-    }
+    }*/
 }
--- services/product/src/main/java/com/andd/ecommerce/exception/ProductPurchaseException.java
+++ services/product/src/main/java/com/andd/ecommerce/exception/ProductPurchaseException.java
@@ -1,9 +1,9 @@
 package com.andd.ecommerce.exception;
 
 public class ProductPurchaseException extends RuntimeException {
+
     public ProductPurchaseException(String msg) {
         super(msg);
     }
-
 
 }
